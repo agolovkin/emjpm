d22e83b050494bc39723c6c919a0ab5fffe38852
Rémi Roycourt <remiroycourt@gmail.com>
2020-06-01 12:01:51 +0200

    feat(enquete): modalite exercice form

---
+++ packages/app/src/components/Enquete/EnqueteModaliteExercice/EnqueteModaliteExercice.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Box } from "rebass";
+
+import { EnqueteModaliteExerciceForm } from "./EnqueteModaliteExerciceForm";
+
+export const EnqueteModaliteExercice = () => {
+  return (
+    <Box>
+      <EnqueteModaliteExerciceForm />
+    </Box>
+  );
+};
+
+export default EnqueteModaliteExercice;
---
+++ packages/app/src/components/Enquete/EnqueteModaliteExercice/index.js
@@ -0,0 +1,1 @@
+export { EnqueteModaliteExercice } from "./EnqueteModaliteExercice";
